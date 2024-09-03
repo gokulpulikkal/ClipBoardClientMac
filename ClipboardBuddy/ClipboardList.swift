fb05bb1fc0f7da33376fdf971fe90201b7aef878
Gokul <gokulplkl@gmail.com>
2024-09-03 17:42:10 -0400

    added iphone and ipad clipboard item list

--- ClipboardBuddy/ClipboardList.swift
+++ ClipboardBuddy/ClipboardList.swift
@@ -12,9 +12,64 @@ struct ClipboardList: View {
     @Environment(\.modelContext) private var modelContext
     @Query(StringItem.sortedByDate()) private var items: [StringItem]
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
+    private var columns = [
+        GridItem(.adaptive(minimum: 350, maximum: 400), spacing: 15)
+    ]
 
     var body: some View {
-        Text("Hojsnfogaskjnfdb")
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(items) { item in
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(item.timestamp.formatted(date: .numeric, time: .standard))
+                            .font(.footnote)
+                            .opacity(0.7)
+                        HStack {
+                            Text(item.value)
+                                .lineLimit(3)
+                                .help(Text(item.value))
+                            Spacer()
+                            HStack {
+                                Button(action: {
+                                    //                            addItemToPastBoard(item: item)
+                                }, label: {
+                                    Image(systemName: "document.on.document")
+                                })
+
+                                Button(action: {
+                                    deleteItem(item: item)
+                                }, label: {
+                                    Image(systemName: "trash")
+                                })
+                            }
+                        }
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 10).fill(.white.shadow(.drop(
+                        color: .black.opacity(0.3),
+                        radius: 3
+                    ))))
+                    .padding(.vertical, 5)
+                }
+            }
+            .animation(.spring, value: items)
+            .padding()
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+
+    private func deleteItem(item: StringItem) {
+        withAnimation {
+            modelContext.delete(item)
+        }
     }
 }
 
--- ClipboardBuddy/LaunchView.swift
+++ ClipboardBuddy/LaunchView.swift
@@ -45,4 +45,5 @@ struct LaunchView: View {
 
 #Preview {
     LaunchView()
+        .modelContainer(previewContainer)
 }
